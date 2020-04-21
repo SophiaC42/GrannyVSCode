4bada8562f25cd19b20ff53704992d956083cbe9
SophiaC42 <carpes4@uw.edu>
2020-04-21 13:23:23 -0700

    testing remote

--- program.cs
+++ program.cs
@@ -6,7 +6,7 @@ namespace VSCodeAttempt2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! Ha, I have a remote!");
         }
     }
 }
